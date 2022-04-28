54fd11b53626d980f9fc6c2e4b81b4d20ea3d250
stiggu <abelmunda@hotmail.com>
2022-04-28 17:06:16 -0500

    class progress

--- exercise-two.js
+++ exercise-two.js
@@ -1,9 +1,9 @@
 ﻿/* Theory questions!
 * 
-* 1. null || 2 || undefined = 2
-* 2. alert(alert(1) || 2 || alert(3)) = Alerts 1 and Alerts 2
-* 3. alert(alert(1) && alert(2)) = Alerts 1
-* 4. alert(null || 2 && 3 || 4) = Alerts 3
+* 1. null || 2 || undefined                 = 2
+* 2. alert(alert(1) || 2 || alert(3))       = Alerts 1 and Alerts 2
+* 3. alert(alert(1) && alert(2))            = Alerts 1, Alerts undefined
+* 4. alert(null || 2 && 3 || 4)             = Alerts 3
 * 5. Done down in the script
 * 6. alert(alert(null) ?? null ?? 2 ?? alert(3)) = alert(null) then alert(2)
 * 7. if(-1||0) alert('first') = Executes
@@ -27,3 +27,21 @@
 // JS needs chain conditions like in python :(
 const between = (age) => (14 <= age) === (age <= 90);
 console.log(between(90))
+
+
+// Class stuff
+const r = [1, 2, 4, 7, 4, 2, 1, 2, 1];
+const sort = (arr) => arr.reduce((acc, previous) => arr.filter(item => item === previous).length === 1 ? previous : acc, undefined);
+console.log(sort(r));
+
+
+const delay = (ms) => new Promise((re, rej) => setTimeout(re, ms));
+delay(3000).then(() => console.log('\n\n\nreturns after 3s'));
+
+console.log(1);
+setTimeout(() => console.log(2));
+Promise.resolve().then(() => console.log(3));
+Promise.resolve().then(() => setTimeout(() => console.log(4)));
+Promise.resolve().then(() => console.log(5));
+setTimeout(() => console.log(6));
+console.log(7);
