f30728c7f456b68631a35850e44a8abbd8c4ca4e
stiggu <abelmunda@hotmail.com>
2022-04-29 16:53:47 -0500

    Update with class challenge, change a bit(lot) of code

---
+++ class-tests/classes-javascript.js
@@ -0,0 +1,30 @@
+﻿class HttpError extends Error {
+    constructor(response) {
+        super(`${response.status} for ${response.url}`);
+        this.name = 'HttpError';
+        this.response = response;
+    }
+}
+
+async function search(url){
+    try{
+        const r = await fetch(url);
+        return r.json();
+    } catch (e) {
+        return new HttpError(r);
+    }
+}
+
+async function demoGithubUser() {
+    let name = "vanimar";
+    try {
+        const githubUser = await search(`https://api.github.com/users/${name}`);
+        console.log(githubUser.name);
+    } catch (e) {
+        if(e instanceof HttpError && e.response.status === 404){
+            console.log('No user found under that userid');
+        }
+    }
+}
+
+demoGithubUser();
