0545e901e4cc5858228e5171c86cf659bd7db3d3
stiggu <abelmunda@hotmail.com>
2022-05-03 13:19:49 -0500

    Third homework

---
+++ exercises/exercise-three.js
@@ -0,0 +1,25 @@
+﻿setTimeout(function () {
+    console.log(1); // Third frame
+});
+
+console.log(2); // Immediately
+
+Promise.resolve().then(function () {
+    console.log(3); // 2nd frame
+});
+
+Promise.resolve().then(function () {
+    setTimeout(function () {
+        console.log(4); // Fourth Frame
+    })
+})
+
+setTimeout(function(){
+    console.log(5); // Third frame
+})
+
+console.log(6); // Immediately
+
+Promise.resolve().then(function(){
+    console.log(7); // 2nd frame
+})
