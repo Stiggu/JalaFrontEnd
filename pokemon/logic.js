3bc76636b3f1841e7b97dd7a8e639ad29169d311
Abel Mundarain <abel.mundarain@jala-foundation.org>
2022-05-06 17:02:08 -0500

    pokemon exercise

---
+++ pokemon/logic.js
@@ -0,0 +1,21 @@
+﻿class Pokemon extends HTMLElement{
+
+    constructor() {
+        super();
+        
+        const tag = document.createElement("p");
+        const text = document.createTextNode("aaaaaaaaaaaa");
+        tag.appendChild(text);
+        const e = document.getElementById("app");
+        e.appendChild(tag);
+    }
+
+}
+
+customElements.define('pokemon-card', Pokemon);
+
+for(let x = 0; x < dataPokemons["results"].length; x++){
+
+    const e = new Pokemon();
+    console.log(e);
+}
