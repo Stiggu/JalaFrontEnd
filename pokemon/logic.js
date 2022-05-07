edeb18497edd831e405b1c19d39b37750f4b1c4f
Abel Mundarain <abel.mundarain@jala-foundation.org>
2022-05-06 23:30:20 -0500

    cards finished, i dont remember how original looked like so i ended up making my own

--- pokemon/logic.js
+++ pokemon/logic.js
@@ -1,21 +1,18 @@
 ﻿class Pokemon extends HTMLElement{
-
-    constructor() {
+    constructor(name, avatar, colour) {
         super();
-        
-        const tag = document.createElement("p");
-        const text = document.createTextNode("aaaaaaaaaaaa");
-        tag.appendChild(text);
-        const e = document.getElementById("app");
-        e.appendChild(tag);
+        let pokemonTemplate = document.getElementById('card-template');
+        let card = pokemonTemplate.content.cloneNode(true);
+        card.querySelector('.container>#avatar').style.backgroundColor = colour;
+        card.querySelector('.container>#avatar>.avatar').setAttribute('src', avatar);
+        card.querySelector('.container>#name>.name').innerText = name.toUpperCase();
+        const pc = document.getElementById("pokemon-container");
+        pc.appendChild(card);
     }
-
 }
 
 customElements.define('pokemon-card', Pokemon);
 
 for(let x = 0; x < dataPokemons["results"].length; x++){
-
-    const e = new Pokemon();
-    console.log(e);
+    new Pokemon(dataPokemons["results"][x].name, getPokemonImageUri(x+1), pokemonColorMap[x+1]);
 }
