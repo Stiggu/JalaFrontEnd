d5d0061fbcd60e633764e84d97c5ffb3ac61f1bc
Abel Mundarain <abelmunda@hotmail.com>
2022-05-19 17:00:04 -0500

    [CLASSEND] Grunt

---
+++ pokemon/Gruntfile.js
@@ -0,0 +1,24 @@
+﻿module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*!<%= grunt.template.today("yyyy-mm-dd") %> */\n',
+            },
+            build: {
+                files: [{
+                    // 'dist/js/logic.min.js': 'src/logic.js',
+                    // 'dist/js/utils.min.js': 'src/utils.js',
+                    // 'dist/js/compiled.min.js': 'src/*.js',
+                    cwd:'./src',
+                    src:'**/*.js',
+                    dest:'./dist/js/*.js'
+                }]
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['uglify']);
+}
---
+++ pokemon/src/logic.js
@@ -0,0 +1,23 @@
+﻿class Pokemon extends HTMLElement{
+    constructor(name, avatar, colour) {
+        super();
+        let pokemonTemplate = document.getElementById('card-template');
+        let card = pokemonTemplate.content.cloneNode(true);
+        card.querySelector('.poke-container').style.backgroundColor = colour;
+        card.querySelector('.poke-container>#avatar').style.backgroundColor = "rgba(0,0,0,0.1)";
+        card.querySelector('.poke-container>#avatar>.avatar').setAttribute('src', avatar);
+        card.querySelector('.poke-container>#name>.name').innerText = name.toUpperCase();
+        const pc = document.getElementById("pokemon-container");
+        pc.appendChild(card);
+    }
+}
+
+customElements.define('pokemon-card', Pokemon);
+
+fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=25')
+.then(res => res.json())
+.then(data => {
+    for(let x = 0; x < data["results"].length; x++){
+        new Pokemon(data["results"][x].name, getPokemonImageUri(x+1), pokemonColorMap[x+1]);
+    }
+})
