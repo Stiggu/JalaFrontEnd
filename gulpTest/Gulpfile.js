a6d63007bc69df0c6e9cfaad937dd732fcd95f3a
Abel Mundarain <abelmunda@hotmail.com>
2022-05-20 21:29:51 -0500

    injection

--- gulpTest/Gulpfile.js
+++ gulpTest/Gulpfile.js
@@ -4,6 +4,8 @@ const sass = require('gulp-sass')(require('node-sass'))
 const del = require('del')
 const uglify = require('gulp-uglify')
 const rename = require('gulp-rename')
+const inject = require('gulp-inject')
+const es = require('event-stream')
 
 function concatMinify(cb) {
     src('./src/*.js')
@@ -30,9 +32,26 @@ function generateCSS(cb) {
     cb();
 }
 
+const gCSS = src(['./src/sass/*.sass', './src/sass/*.scss'])
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(dest('./dist/css/'));
+
+const ugly = src('./src/*.js')
+    .pipe(concat('app.js'))
+    .pipe(dest('./dist/js/'))
+    .pipe(rename('ugly.min.js'))
+    .pipe(uglify())
+    .pipe(dest('./dist/js/'));
+
 function cleanFolders(cb) {
     del('dist/**', {force: true})
     cb();
 }
 
-exports.default = series(cleanFolders, generateCSS, concatMinify, uglification);
+function injectTohtml(cb){
+    src('./src/mainBootstrap.html')
+        .pipe(inject(es.merge(ugly, gCSS), { relative:true }))
+        .pipe(dest('./dist'))
+    cb();
+}
+exports.default = series(cleanFolders, injectTohtml);
