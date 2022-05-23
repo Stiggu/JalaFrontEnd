936ebf63742d171b52a352445fdf556a07ffd02b
Abel Mundarain <abelmunda@hotmail.com>
2022-05-23 17:04:23 -0500

    [CLASS END] Webpack class

---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,13 @@
+﻿const path = require('path')
+
+module.exports = {
+    mode: 'none',
+    entry: {
+        app: './src/index.js',
+    },
+    output: {
+        // filename: 'bundle.js'
+        filename: '[name].js',
+        path: path.join(__dirname, 'build')
+    }
+}
