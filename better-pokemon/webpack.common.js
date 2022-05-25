da0a759a8a7a5407eddcd807a1ac721ecbd1b349
Abel Mundarain <abelmunda@hotmail.com>
2022-05-25 17:07:40 -0500

    [CLASS END] Pokemon webpack port

---
+++ better-pokemon/webpack.common.js
@@ -0,0 +1,25 @@
+﻿const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: ['./src/logic.js'],
+    plugins: [
+        new htmlWebpackPlugin({
+            inject: 'body',
+            template: 'index.html',
+            filename: 'index.html',
+            scriptLoading: "blocking"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+}
---
+++ better-pokemon/webpack.prod.js
@@ -0,0 +1,10 @@
+﻿const {merge} = require('webpack-merge');
+const common = require("./webpack.common");
+
+module.exports = merge(common, {
+    mode:'production',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/build'
+    },
+})
