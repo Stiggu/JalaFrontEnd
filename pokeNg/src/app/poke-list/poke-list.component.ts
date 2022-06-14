f5e3f38cd6655169275ff684161dddb119e451c3
Abel Mundarain <abelmunda@hotmail.com>
2022-06-14 16:45:37 -0500

    angular 1st challenge update

--- pokeNg/src/app/poke-list/poke-list.component.ts
+++ pokeNg/src/app/poke-list/poke-list.component.ts
@@ -15,16 +15,23 @@ export class PokeListComponent {
   todoList: ITodoItems[] = [];
   itemToAdd: string = '';
 
-  constructor() {
-    this.todoList = [];
-  }
-
   addItem(){
-    if(this.itemToAdd.length === 0) return;
+    if(!this.itemToAdd) return;
     this.todoList.push({
       text: this.itemToAdd,
       id: this.todoList.length,
     });
     this.itemToAdd = '';
+  }
+
+  completeItem(id: string){
+    const ele: HTMLElement | null = document.getElementById(id);
+    if(ele){
+      ele.style.textDecoration = "line-through"
+    }
+  }
+
+  deleteItem(id: number){
+    this.todoList.splice(id-1, 1);
   }
 }
