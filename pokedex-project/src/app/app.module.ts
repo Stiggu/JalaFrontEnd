aa068d31b8c6063684ea74e601bc43051e0edd2e
Abel Mundarain <abelmunda@hotmail.com>
2022-06-17 17:26:35 -0500

    Added utils file Added the poke-card-list component Added the poke-card component

--- pokedex-project/src/app/app.module.ts
+++ pokedex-project/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PokeCardListComponent } from './poke-card-list/poke-card-list.component';
+import { PokeCardComponent } from './poke-card/poke-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokeCardListComponent,
+    PokeCardComponent
   ],
   imports: [
     BrowserModule,
