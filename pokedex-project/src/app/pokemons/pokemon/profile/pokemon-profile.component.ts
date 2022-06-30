a8d2160b1f472e449f76be8d00163dbbcb347555
Abel Mundarain <abelmunda@hotmail.com>
2022-06-30 16:58:05 -0500

    - Go back with navigation

--- pokedex-project/src/app/pokemons/pokemon/profile/pokemon-profile.component.ts
+++ pokedex-project/src/app/pokemons/pokemon/profile/pokemon-profile.component.ts
@@ -1,9 +1,18 @@
 ﻿import {Component} from "@angular/core";
+import {Location} from "@angular/common";
 
 @Component({
   selector: 'pokemon-profile',
   templateUrl: './pokemon-profile.component.html'
 })
 export class PokemonProfileComponent {
+  id: string = '1';
+  fields: any;
 
+  constructor(private location: Location) {
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
