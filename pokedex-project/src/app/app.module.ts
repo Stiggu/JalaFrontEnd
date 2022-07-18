a627701ae32565ca8abc61f7daba98d55c6f91d1
Abel Mundarain <abelmunda@hotmail.com>
2022-07-18 16:51:08 -0500

    - PWA start

--- pokedex-project/src/app/app.module.ts
+++ pokedex-project/src/app/app.module.ts
@@ -6,6 +6,8 @@ import {HttpClientModule} from "@angular/common/http";
 import {AppRoutingModule} from "./app.routing.module";
 import {PokemonModule} from "./pokemons/pokemon.module";
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
         HttpClientModule,
         PokemonModule,
         FontAwesomeModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: environment.production,
+          // Register the ServiceWorker as soon as the application is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        }),
     ],
   providers: [],
   exports: [],
